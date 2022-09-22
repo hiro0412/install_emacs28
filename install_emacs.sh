e696e41087942e2b6d52f64dc84f4410763c74c7
Hiroaki Honda <from.endor@gmail.com>
2022-09-22 10:08:40 +0900

    Make install_emacs.sh compatible with Mac.

--- install_emacs.sh
+++ install_emacs.sh
@@ -1,10 +1,24 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env bash -e
 
 cd
 git clone --single-branch -b emacs-28 --depth 1 https://github.com/emacs-mirror/emacs.git
 cd emacs
 ./autogen.sh
-./configure --prefix=$HOME/.local --with-native-compilation --with-json --with-xft --with-x-toolkit=gtk3 CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
+
+if [ $(uname | tr '[A-Z]' '[a-z]') == 'darwin' ];
+then
+    CFGOPTS_DEPEND_OS="--disable-ns-selfcontained"
+else
+    CFGOPTS_DEPEND_OS="--with-x-toolkit=gtk3"
+fi
+
+./configure \
+    --prefix=$HOME/.local \
+    --with-native-compilation \
+    --with-json \
+    --with-xft \
+    $CFGOPTS_DEPEND_OS \
+    CFLAGS="-O3 -mtune=native -march=native -fomit-frame-pointer"
 
 make -j24
 make install
